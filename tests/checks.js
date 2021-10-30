8b31e5c052fac31ead9eace10c4c95a47f016d58
sonsoleslp <poynting.slp@gmail.com>
2021-10-30 10:44:25 +0200

    Update

--- tests/checks.js
+++ tests/checks.js
@@ -34,7 +34,7 @@ describe('Commit', function () {
         this.msg_err = "No se ha encontrado el fichero 'git_account' que debe contener el nombre de usuario de github";
 
         student = fs.readFileSync(path.join(PATH_ASSIGNMENT, 'git_account'), {encoding: 'utf8'}).replace(/^\s+|\s+$/g, '');;
-        REPO_URL = `git://github.com/${student}/${REPO_NAME}.git`;
+        REPO_URL = `git@github.com:${student}/${REPO_NAME}.git`;
         this.msg_ok = `Se ha encontrado el fichero 'git_account': ${student}`;
         should.exist(student);
     });
